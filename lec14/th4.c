cda1d48c5104b42c9c2661657d4c11b68dc9d4ee
kwaksj329 <kwaksj329@ajou.ac.kr>
2022-01-26 15:58:06 +0900

    Added lec14

--- lec14/th4.c
+++ lec14/th4.c
@@ -1,8 +1,6 @@
-#include <pthread.h>
 #include <stdio.h>
 #include <sys/types.h>
 #include <unistd.h>
-
 
 int bbb = 0; 
 
@@ -10,12 +8,13 @@ void fn_s()
 {
     static int a = 0; 
     printf("== %d %d ==",a++, bbb++);
+    fflush();
 }
 
 
 void *run (void *arg)
 {
-    printf("Hello world of POSIX threads.%d\n", (int)(0) );
+    printf("Hello world of POSIX threads.%d\n", (int) (0) );
     for (int i = 0; i < 10; i++)
 	{
 		sleep(1); 
@@ -27,13 +26,10 @@ void *run (void *arg)
 
 int main()
 {
-	char bufff[10];
-    	setvbuf(stdout, bufff,  _IOFBF,  10);
-	//pthread_t thread1;
 	int result1;
-	//pthread_create(&thread1, NULL, run, NULL );
+    	char bufff[10]; 
+    	setvbuf(stdout,bufff,  _IONBF, 10); 
 	run((void *) 0); 
-	//pthread_join(thread1, (void **) &result1);
 	printf("Thread return %d at the end\n", result1);
 }
 
